2cd475e961bd93f9b0fd8ba7fcbe3293ebac556d
jasperdeklijn <54976517+jasperdeklijn@users.noreply.github.com>
2023-11-23 13:50:52 +0100

    dit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state = {
+    products: []
+  };
+
+  async componentDidMount() {
+    const response = await fetch('/products');
+    const body = await response.json();
+    this.setState({products: body});
+  }
+
+  render() {
+    const {products} = this.state;
+    return (
+        <div className="App">
+          <header className="App-header">
+            <div className="App-intro">
+              <h2>products</h2>
+              {products.map(product =>
+                  <div key={product.id}>
+                    {product.productname} ({product.price})
+                  </div>
+              )}
+            </div>
+          </header>
+        </div>
+    );
+  }
 }
-
 export default App;
